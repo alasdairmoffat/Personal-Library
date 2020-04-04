2ff84d6750eaae0264cad19c6d9bcae3823a0447
Alasdair Moffat <contact@alasdairmoffat.com>
2020-04-04 14:28:02 +0100

    Security Fixes

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
   .then(console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
